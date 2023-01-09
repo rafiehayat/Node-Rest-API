efd931fbd7eaf7b5a29a8a5d84303cc55e7c6522
Hayat <rafiehayat5@gmail.com>
2023-01-09 20:30:50 +0530

    2

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -16,6 +18,14 @@ mongoose.connect(
   }
 );
 
-app.listen(8800, () => {
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(5000, () => {
   console.log("Backend server is running");
 });
