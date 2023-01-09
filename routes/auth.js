5d26c1559eddcdd1d99df9aababc60c2954bf76e
Hayat <rafiehayat5@gmail.com>
2023-01-10 00:11:42 +0530

    send and get request

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,42 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/register", async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   try {
-    const user = await new User({
-      username: "Hayat",
-      email: "hayat@gmail.com",
-      password: "123456",
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
     });
 
-    await user.save();
-    res.send("ok");
+    const user = await newUser.save();
+    res.status(200).json(user);
   } catch (err) {
     console.log(err);
     next(err);
+  }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("incorrect Password");
+
+    res.status(200).json(user);
+  } catch {
+    console.log(err);
   }
 });
 
