51c65fe7f3018a67d41a3d7a3ddcd9ac17ef8d0a
Hayat <rafiehayat5@gmail.com>
2023-01-12 21:55:52 +0530

    ...

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(5000, () => {
+app.listen(8800, () => {
   console.log("Backend server is running");
 });
