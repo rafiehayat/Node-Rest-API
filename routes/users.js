d7847cdbf5e485d4f92bc1a4fcae7b9010c06ccb
Hayat <rafiehayat5@gmail.com>
2023-01-13 21:53:17 +0530

    ....

--- routes/users.js
+++ routes/users.js
@@ -41,9 +41,13 @@ router.delete("/:id", async (req, res, next) => {
 });
 
 //get a user
-router.get("/:id", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
   try {
-    const user = await User.findById(req.params.id);
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
     const { password, updatedAt, ...other } = user._doc;
     res.status(200).json(other);
   } catch (err) {
